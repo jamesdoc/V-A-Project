2d6e30ea880073a12ee0b1fd1579ba5808bb53a1
James Doc <Doc@Andromeda.local>
2013-09-23 17:53:31 +0100

    Output formatted JSON
    JSON printed into an HTML table
    
    Not elegant at the moment, but functional

--- va_api.py
+++ va_api.py
@@ -3,12 +3,26 @@
 import urllib2, json
 
 #base_url = 'http://www.vam.ac.uk/api/json/museumobject/search?';
-base_url = 'http://www.vam.ac.uk/api/json/museumobject/search?materialsearch=gold'
+base_url = 'http://www.vam.ac.uk/api/json/museumobject/search?materialsearch=gold&limit=10'
 
 response = urllib2.urlopen(base_url)
 
 json_object = json.loads(response.read())
 
-for item in json_object['records']:   
-        print item;\
-        print ;\
+print '<table>';\
+print '<thead><tr><th>Name</th><th>Location</th></tr></thead>';\
+
+for item in json_object['records']: 
+    
+    print '<tr>';\
+    
+    if item['fields']['title']:
+        print '<td>' + item['fields']['title'] + ' - ' + item['fields']['object'] + '</td>';\
+        
+    else:
+        print '<td>' + item['fields']['object'] + '</td>';\
+    
+    print '<td>' + item['fields']['location'] + '</td>';\
+    print '</tr>';\
+    
+print '</table>';\
